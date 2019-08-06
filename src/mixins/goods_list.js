2e99ec39641c714caf7cdc231e39fa33709614a6
Littlebaltan <littlebaltan@163.com>
2019-08-06 12:04:01 +0800

    feat(master):pullDown

--- src/mixins/goods_list.js
+++ src/mixins/goods_list.js
@@ -10,7 +10,11 @@ export default class extends wepy.mixin {
     // 每页显示多少条数据
     pagesize: 10,
     total: '',
-    goodsList: []
+    goodsList: [],
+    // 底线的显示与隐藏
+    isover: false,
+    // 表示当前数据是否正在请求中
+    isloading: false
   }
   onLoad(options) {
     console.log(options)
@@ -21,15 +25,47 @@ export default class extends wepy.mixin {
 
   // 触底操作
   onReachBottom() {
+    // 判断是否正在请求数据中
+    if (this.isloading) {
+      return
+    }
     // 先判断是会否有下一页的数据
-    if (this.pagenum * this.pagesize >= this.total) return
+    if (this.pagenum * this.pagesize >= this.total) {
+      this.isover = true
+      return
+    }
     console.log('触底了')
     this.pagenum++
     this.getGoodsList()
   }
 
+  // 下拉刷新的操作
+  onPullDownRefresh() {
+    // 初始化必要的字段值
+    this.pagenum = 1
+    this.total = 0
+    this.goodsList = []
+    this.isover = this.isloading = false
+    // 重新发起数据请求
+    this.getGoodsList(() => {
+      console.warn('这是下拉刷新的关闭')
+      // 停止下拉刷新
+      wepy.stopPullDownRefresh()
+    })
+  }
+
+  methods = {
+    // 点击跳转商品详情页面
+    goGoodsDetaill(goods_id) {
+      wepy.navigateTo({
+        url: '/pages/goods_detail/main?goods_id=' + goods_id
+      })
+    }
+  }
+
   // 获取商品列表数据
-  async getGoodsList() {
+  async getGoodsList(cb) {
+    this.isloading = true
     const { data: res } = await wepy.get('/goods/search', {
       query: this.query,
       cid: this.cid,
@@ -42,7 +78,8 @@ export default class extends wepy.mixin {
     }
     this.goodsList = [...this.goodsList, ...res.message.goods]
     this.total = res.message.total
+    this.isloading = false
     this.$apply()
-    console.log(res)
+    cb && cb()
   }
 }
